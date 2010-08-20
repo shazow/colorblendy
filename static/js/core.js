af30727892c2d924ba3654f43324a143f8ce2505
Andrey Petrov <andrey.petrov@shazow.net>
2010-08-19 17:55:59 -0700

    Added rgb_to_hsv function.

--- static/js/core.js
+++ static/js/core.js
@@ -39,6 +39,27 @@ function rgb_to_css(rgb) {
     return 'rgb(' + rgb[0] + ',' + rgb[1] +',' + rgb[2] + ')';
 }
 
+function rgb_to_hsv(rgb){
+    /* [255,255,255] -> [0,0,1] */
+    var r = rgb[0]/255, g = rgb[1]/255, b = rgb[2]/255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, v = max;
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if(max == min) h = 0;
+    else {
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return [h, s, v];
+}
+
+
 function apply_filter(c1, c2, fn) {
     return [fn(c1[0], c2[0]),
             fn(c1[1], c2[1]),
