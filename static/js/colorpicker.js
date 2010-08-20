d482b93641c134daa2144d2e51bd9056ec14b7d0
Andrey Petrov <andrey.petrov@shazow.net>
2010-08-19 17:12:06 -0700

    More unobfuscation and commenting by John.

--- static/js/colorpicker.js
+++ static/js/colorpicker.js
@@ -1,3 +1,7 @@
+//
+// Set the color of the pixel in img at coordinates x and y to the color
+// provided
+//
 function setPixel(img, x, y, r, g, b) {
     var i = (x + y * img.width) * 4;
     img.data[i+0] = r;
@@ -6,7 +10,11 @@ function setPixel(img, x, y, r, g, b) {
     img.data[i+3] = 0xff;
 }
 
-function drawSquareGradient(img, rbase, gbase, bbase) {
+//
+// Accepts a "base" color and builds a square gradient with that color, black,
+// and white.
+//
+function generatePickerSquare(img, rbase, gbase, bbase) {
     var i,j,r,g,b,size,coeff,x,y;
     size=img.width;
     coeff=0x100/size;
@@ -22,57 +30,78 @@ function drawSquareGradient(img, rbase, gbase, bbase) {
     }
 }
 
-function getSpectrumColor(color, x, w) {
-    var f,v,t,n;
-    f = 1.0/0x100;
-    v = (x/w)*0x600;
-    if (v<0x100) {
-        t=v;
-        n=[1,f*t,0];
-    } else if (v<0x200) {
-        t=v-0x100;
-        n=[1-f*t,1,0];
-    } else if (v<0x300) {
-        t=v-0x200;
-        n=[0,1,f*t];
-    } else if (v<0x400) {
-        t=v-0x300;
-        n=[0,1-f*t,1];
-    } else if (v<0x500) {
-        t=v-0x400;
-        n=[f*t,0,1];
+//
+// Given a coefficient x in the range [0,1), calculate the color in the rainbow
+// spectrum that should appear at that spot.
+//
+// The rule for the calculation is that at least 1 color must be at 100% at all
+// times, but no more than 2 colors are active at any one time, and all
+// permutations satisfying this requirement are in the spectrum
+//
+function getSpectrumColor(color, x) {
+    var t,n;
+    t = x*6.0;
+    if (t<1) {
+        t=t;
+        n=[1,t,0];
+    } else if (t<2) {
+        t=t-1.0;
+        n=[1-t,1,0];
+    } else if (t<3) {
+        t=t-2.0;
+        n=[0,1,t];
+    } else if (t<4) {
+        t=t-3.0;
+        n=[0,1-t,1];
+    } else if (t<5) {
+        t=t-4.0;
+        n=[t,0,1];
     } else {
-        t=v-0x500;
-        n=[1,0,1-f*t];
+        t=t-5.0;
+        n=[1,0,1-t];
     }
     color[0]=n[0];
     color[1]=n[1];
     color[2]=n[2];
 }
 
-function drawSpectrumBar(ctxb,m,x) {
-    var color=[0,0,0],f=0x100,w=m.width,h=m.height;
+//
+// Given a canvas context ctx, a pixel array m, and an active location x in the
+// range [0,1), draw the spectrum bar on the canvas and put a black line at
+// location x.
+//
+function drawSpectrumBar(ctx,m,x) {
+    var color=[0,0,0],factor=0x100,w=m.width,h=m.height;
+    // Loop through the canvas
     for(i=0; i<w; i++) {
-        getSpectrumColor(color, i, w);
+        // Get the color that should be at this location
+        getSpectrumColor(color, i/w);
         for (j=0; j<h; j++) {
-            setPixel(m, i, j, color[0]*f, color[1]*f, color[2]*f);
+            setPixel(m, i, j, color[0]*factor, color[1]*factor, color[2]*factor);
         }
     }
-    ctxb.putImageData(m,0,0);
-    ctxb.beginPath();
-    ctxb.moveTo(parseInt(x*w),0);
-    ctxb.lineTo(parseInt(x*w),h);
-    ctxb.closePath();
-    ctxb.strokeStyle = "#000000";
-    ctxb.lineWidth = 3;
-    ctxb.stroke();
+    // Render the image array to the canvas.
+    ctx.putImageData(m,0,0);
+
+    // Draw the line.
+    ctx.beginPath();
+    ctx.moveTo(parseInt(x*w),0);
+    ctx.lineTo(parseInt(x*w),h);
+    ctx.closePath();
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 3;
+    ctx.stroke();
 }
 
-function drawFocusCircle(ctx,r,x,y,co) {
+//
+// Draw a circle on the provided context with radius r, centered at (x,y), and
+// styled with the color string represented by color.
+//
+function drawFocusCircle(ctx,r,x,y,color) {
     ctx.beginPath();
     ctx.arc(x,y,r,0,Math.PI*2,true);
     ctx.closePath();
-    ctx.strokeStyle = co;
+    ctx.strokeStyle = color;
     ctx.lineWidth = 2;
     ctx.stroke();
 }
@@ -82,7 +111,8 @@ var img, color;
 $(document).ready(function() {
     var e,s,loc=[0,0],col=0;
 
-    e = $("#pickerbase").get(0);
+    // Generate the spectrum bar.
+    e = $("#spectrumbar").get(0);
     ctxb = e.getContext("2d");
 
     w = parseInt(e.getAttribute("width"));
@@ -92,6 +122,7 @@ $(document).ready(function() {
     drawSpectrumBar(ctxb,m,col);
     ctxb.putImageData(m, 0, 0);
 
+    // Generate the picker square.
     e = $("#pickersquare").get(0);
     ctx = e.getContext("2d");
 
@@ -99,13 +130,13 @@ $(document).ready(function() {
 
     img = ctx.createImageData(s, s);
 
-    var tfun = function(coeff) {
+    var regeneratePickerSquare = function(coeff) {
         var color=[];
         getSpectrumColor(color,coeff,1);
-        drawSquareGradient(img,color[0],color[1],color[2]);
+        generatePickerSquare(img,color[0],color[1],color[2]);
     }
 
-    var rfun = function() {
+    var redrawPickerSquare = function() {
         var idx=(loc[0] + loc[1] * m.width) * 4;
         var r=img.data[idx],
             g=img.data[idx+1],
@@ -116,25 +147,26 @@ $(document).ready(function() {
         drawFocusCircle(ctx, 8, loc[0], loc[1], "#000000");
     };
 
-    var trfun = function() {
-        tfun(col); rfun();
+    var regenRedrawPicker = function() {
+        regeneratePickerSquare(col);
+        redrawPickerSquare();
     };
 
     var cfun = function(ev) {
         os=$("#pickersquare").offset();
         loc[0]=ev.pageX - os.left;
         loc[1]=ev.pageY - os.top;
-        rfun();
+        redrawPickerSquare();
     };
 
     var calcSpectrumOffset = function(ev) {
-            os=$("#pickerbase").offset();
-            col=(ev.pageX-os.left)/m.width;
-            drawSpectrumBar(ctxb,m,col);
-            trfun();
-        };
+        os=$("#spectrumbar").offset();
+        col=(ev.pageX-os.left)/m.width;
+        drawSpectrumBar(ctxb,m,col);
+        regenRedrawPicker();
+    };
 
-    trfun();
+    regenRedrawPicker();
 
     var sqDrag=false;
     var pDrag=false;
@@ -146,11 +178,11 @@ $(document).ready(function() {
     $("#pickersquare").mousemove(function(e) {
         if (sqDrag) {cfun(e);}
     });
-    $("#pickerbase").click(calcSpectrumOffset);
-    $("#pickerbase").mousedown(function(e) {
+    $("#spectrumbar").click(calcSpectrumOffset);
+    $("#spectrumbar").mousedown(function(e) {
         pDrag = true;
     });
-    $("#pickerbase").mousemove(function(e) {
+    $("#spectrumbar").mousemove(function(e) {
         if (pDrag) {calcSpectrumOffset(e);}
     });
     $(document).mouseup(function(e) {
