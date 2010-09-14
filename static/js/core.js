9bcb96da660a3006a80870576ff4237b8a81ad48
Andrey Petrov <andrey.petrov@shazow.net>
2010-09-14 00:29:43 -0700

    Updated core functions

--- static/js/core.js
+++ static/js/core.js
@@ -12,20 +12,15 @@ function assure_hash_prefix(s) {
 }
 function hex_to_rgb(hex) {
     /* "FFFFFF" -> [255,255,255] */
-    if(hex[0] == '#') {
-        hex = hex.substring(1,7);
-    }
+    if(hex[0] == '#') hex = hex.substring(1,7);
+    if(hex.length < 6) hex += "000000".substr(0,6-hex.length);
     return [parseInt(hex.substring (0, 2), 16), parseInt(hex.substring(2, 4), 16), parseInt(hex.substring(4, 6), 16)]
 }
 
 function rgb_to_hex(rgb) {
     /* [255,255,255] -> "FFFFFF" */
     var r = (rgb[2] | (rgb[1] << 8) | (rgb[0] << 16)).toString(16);
-
-    // Pad 0's
-    for(var i=0, stop=6-r.length; i<stop; i++) r += "0";
-
-    return r;
+    return "000000".substr(0,6-r.length) + r;
 }
 
 function css_to_rgb(s) {
@@ -35,9 +30,40 @@ function css_to_rgb(s) {
 }
 
 function rgb_to_css(rgb) {
-    /* [255,255,255] -> "rgb(255, 255, 255)" */
+    /* [255,255,255] -> "rgb(255,255,255)" */
     return 'rgb(' + rgb[0] + ',' + rgb[1] +',' + rgb[2] + ')';
 }
+
+function rgb_a_to_css(rgb, a) {
+    /* [255,255,255,0.5] -> "rgba(255,255,255,0.5)" */
+    return 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',' + a + ')';
+}
+
+function rgb_to_hsv(rgb){
+    /* [50,50,100] -> [170, 127.5, 100] */
+    var r = rgb[0]/255, g = rgb[1]/255, b = rgb[2]/255;
+    var max = Math.max(r,g,b), min = Math.min(r,g,b);
+    if(max==min) return [0,0,max*255];
+
+    var v = max, s = (max-min) / max;
+    var rc = (max-r) / (max-min),
+        gc = (max-g) / (max-min),
+        bc = (max-b) / (max-min);
+
+    if(r==max) h = bc-gc;
+    else if(g==max) h = 2+rc-bc;
+    else h = 4.0+gc-rc;
+
+    h = (h/6.0) % 1.0;
+    if(h<0) h += 1.0
+
+    return [h*255,s*255,v*255];
+}
+
+function invert_rgb(rgb) {
+    return [0xff - rgb[0], 0xff - rgb[1], 0xff - rgb[2]];
+}
+ 
 
 function apply_filter(c1, c2, fn) {
     return [fn(c1[0], c2[0]),
