8dfe920a284199f042f46538c2e261fadba463f4
Andrey Petrov <andrey.petrov@shazow.net>
2010-11-15 00:25:30 -0500

    Permalink urls.

--- static/js/core.js
+++ static/js/core.js
@@ -1,35 +1,55 @@
-var reset_pickers, p1, p2;
+var reset_pickers, p1, p2, blend_mode, changing_picker = true, changed_picker = true;
+
+
+function get_blend_mode() {
+    if(!blend_mode) return 'multiply';
+    return blend_mode.text().toLowerCase();
+}
+function write_url() {
+    location.hash = "#!/" + get_blend_mode() + '/' + rgb_to_hex(p1.rgb) + '/' + rgb_to_hex(p2.rgb);
+}
+function read_url() {
+    var hash = location.hash;
+    if(!hash || hash.substring(0,3) != '#!/') return;
+    var parts = location.hash.substring(3).split('/');
+
+    set_mode(parts[0]);
+    p1.set_color(hex_to_rgb(parts[1]));
+    p2.set_color(hex_to_rgb(parts[2]));
+
+    render_blend();
+}
 
 function render_blend() {
-    var blend_mode = $("#blend-mode li.active").text().toLowerCase();
     try {
         $("#color-bottom-preview").css('background-color', assure_hash_prefix($("#color-bottom").val()));
         $("#color-top-preview").css('background-color', assure_hash_prefix($("#color-top").val()));
 
         var c1 = css_to_rgb($("#color-bottom-preview").css('background-color'));
         var c2 = css_to_rgb($("#color-top-preview").css('background-color'));
-        var blended = blend(blend_mode, c1, c2);
+        var blended = blend(get_blend_mode(), c1, c2);
 
         $("#color-blended-preview").css('background-color', rgb_to_css(blended));
         $("#color-blended").val("#" + rgb_to_hex(blended));
     } catch(err) {
         alert("Unsupported color format.");
+        throw err;
     }
+
+    if(!changing_picker) write_url();
+}
+
+function set_mode(mode) {
+    if(blend_mode) $(blend_mode).removeClass('active');
+    blend_mode = $("#blend-mode #mode-" + mode).addClass('active');
+    render_blend();
 }
 
 $(document).ready(function() {
-    var active = $("#blend-mode li.active");
+    blend_mode = $("#blend-mode li.active");
+
     $("#blend-mode li").click(function(e) {
-        if(active) $(active).removeClass('active');
-        active = $(this).addClass('active');
-        render_blend();
-    });
-    $(document).keydown(function(e) {
-        if(e.which == 38 || e.which == 75) { // Down
-            $(active).prev().click();
-        } else if(e.which == 40 || e.which == 74) { // Up
-            $(active).next().click();
-        }
+        set_mode($(this).text().toLowerCase());
     });
 
     p1 = new ColorPicker($("#color-bottom"));
@@ -40,22 +60,23 @@ $(document).ready(function() {
     p2.preview = $("#color-top-preview");
 
     var body = $("body");
-    function disable_text_select() { body.disableTextSelect(); }
-    function enable_text_select() { body.enableTextSelect(); }
 
     var active_picker = false;
 
     $.map([p1, p2], function(o, i) {
         o.target.focus(function() {
-            if(active_picker && active_picker != o.container) {
-                active_picker.hide();
+            if(active_picker && active_picker != o) {
+                active_picker.container.hide();
                 active_picker = false;
+                changed_picker = false;
             }
-            active_picker = o.container.show();
+            o.container.show();
+            active_picker = o;
         }).blur(function() {
             if(!active_picker) return;
-            active_picker.hide();
+            active_picker.container.hide();
             active_picker = false;
+            changed_picker = false;
         });
 
         o.add_listener('change', function(e, rgb) {
@@ -66,8 +87,8 @@ $(document).ready(function() {
             render_blend();
         });
 
-        o.add_listener('dragstart', disable_text_select);
-        o.add_listener('dragstop', enable_text_select);
+        o.add_listener('dragstart', function(e) { body.disableTextSelect(); changing_picker = true; changed_picker = true; });
+        o.add_listener('dragstop', function (e) { body.enableTextSelect(); changing_picker = false; });
     });
 
     reset_pickers = function() {
@@ -77,5 +98,20 @@ $(document).ready(function() {
         p2.set_color(c2);
     }
 
+    read_url();
     reset_pickers();
+    changing_picker = false;
+    changed_picker = false;
+
+    $(document).keydown(function(e) {
+        if(active_picker && changed_picker) {
+            active_picker.target.blur();
+        }
+        if(e.which == 38 || e.which == 75) { // Down
+            $(blend_mode).prev().click();
+        } else if(e.which == 40 || e.which == 74) { // Up
+            $(blend_mode).next().click();
+        }
+    });
+
 });
